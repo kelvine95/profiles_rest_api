78b731c829b82b94574265619b0a9194e3181cc1
Emwinghare Kelvin <ighodaro@bitbucket.org>
2020-12-06 01:27:51 +0100

    add model viewset, permission and filter

--- profiles_rest_api/views.py
+++ profiles_rest_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from . import permissions
 # Create your views here.
+
+from . import serializers, models
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
